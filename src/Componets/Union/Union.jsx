75a868765ee2170bf43ab5cbf6841682d46809f0
Aldos650 <tynctykanarbekov918@gmail.com>
2026-02-09 17:35:05 +0600

    eror

--- src/Componets/Union/Union.jsx
+++ src/Componets/Union/Union.jsx
@@ -1,5 +1,5 @@
 import scss from "./Union.module.scss";
-import Icon from "../../assets/icon.svg";
+import Icon from "../../assets/Icon.svg";
 import { Link } from "react-router-dom";
 
 function Union() {
